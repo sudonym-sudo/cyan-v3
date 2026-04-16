dfc76c7b8694f6d110d7325769dfc64821eb5879
Nicholas M <memecake788@gmail.com>
2026-04-15 20:44:08 -0500

    .

--- src/Sidebar.svelte
+++ src/Sidebar.svelte
@@ -107,7 +107,7 @@
             <div class="logo-area">
                 <div class="site-info">
                     <span class="site-name">CYλN</span>
-                    <span class="version">v3.3</span>
+                    <span class="version">v3.3.1</span>
                 </div>
             </div>
 
